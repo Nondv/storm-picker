1658a74757f911ba3b88c772f7682b34ccf1863d
Dmitriy Non <non.dmitriy@gmail.com>
2018-05-27 01:07:42 +0300

    add filter to the App

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
 import Hero from './ui/Hero'
+import Feature from './entities/Feature'
+import FeatureFilter from './ui/FeatureFilter'
 
 class App extends Component {
-  state = {}
+  state = {
+    filterSelection: {}
+  }
 
   componentDidMount() {
+    axios.get('/features.json')
+         .then(response => this.setState({ features: response.data.map(f => new Feature(f)) }));
     axios.get('/heroes.json')
          .then(response => this.setState({ heroes: response.data }));
   }
 
   render() {
+    const {filterSelection, features} = this.state;
+    const mergeObjects = (a, b) => Object.assign({}, a, b)
+
     return (
       <div className="App">
+        <FeatureFilter features={features || []}
+                       selection={{}}
+                       onChange={c => this.setState({ filterSelection: mergeObjects(filterSelection, c) })} />
         <table className='app__hero-table'>
           <tbody>
             {
@@ -32,7 +44,7 @@ class App extends Component {
   // TODO: cache it
   rowsOfHeroes() {
     const rowLength = 6;
-    const heroes = this.state.heroes || [];
+    const heroes = this.filteredHeroes();
     const result = []
 
     let currentRow = [];
@@ -48,6 +60,25 @@ class App extends Component {
     }
 
     return result
+  }
+
+  filteredHeroes() {
+    const {filterSelection} = this.state;
+    const heroes = this.state.heroes || [];
+    if(!this.isFilterEnabled()) {
+      return heroes;
+    }
+
+    const requiredFeatures = Object.entries(filterSelection)
+                                   .filter(keyvalue => keyvalue[1])
+                                   .map(keyvalue => keyvalue[0]);
+
+    const heroFilter = h => !requiredFeatures.find(f => !h.features[f]);
+    return heroes.filter(heroFilter);
+  }
+
+  isFilterEnabled() {
+    return -1 !== Object.values(this.state.filterSelection).indexOf(true);
   }
 }
 
