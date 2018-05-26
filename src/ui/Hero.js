eb14b15e3a1edd1dc042ce1812d0d5655e622fa6
Dmitriy Non <non.dmitriy@gmail.com>
2018-05-26 22:48:27 +0300

    add feature list to Hero

--- src/ui/Hero.js
+++ src/ui/Hero.js
@@ -5,6 +5,9 @@ const Hero = function({data}) {
   return (
     <div className='hero'>
       <img className='hero__bust' alt={data.name} src={data.images.bust} />
+      <ul>
+        {Object.keys(data.features).map(f => <li key={f}>{f}</li>)}
+      </ul>
     </div>
   )
 }
