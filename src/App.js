81ddd9c25e263be17b66bb85fea4f84909ca7c95
Dmitriy Non <non.dmitriy@gmail.com>
2018-05-26 20:20:47 +0300

    replace App content with heroes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
 import './App.css';
+import Hero from './ui/Hero'
 
 class App extends Component {
+  state = {}
+
+  componentDidMount() {
+    axios.get('/heroes.json')
+         .then(response => this.setState({ heroes: response.data }));
+  }
+
   render() {
+    const heroes = this.state.heroes || [];
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          {heroes.map(h => <Hero key={h.name} data={h} />)}
+        </div>
       </div>
     );
   }
