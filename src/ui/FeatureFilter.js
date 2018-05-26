354e874704b700f7d56ed0c0541321fb2cddcabd
Dmitriy Non <non.dmitriy@gmail.com>
2018-05-27 01:29:40 +0300

    make show features by columns in FeatureFilter

--- src/ui/FeatureFilter.js
+++ src/ui/FeatureFilter.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types'; // ES6
+import arrayToTable from '../util/arrayToTable';
 
 const Feature = function({data, selected, onChange}) {
   return (
     <div className='feature-filter__feature'>
-      <input type="checkbox" checked={selected} onChange={e => onChange(e.target.checked)} />
-      <span>{data.title}</span>
+      <label>
+        <input type="checkbox" checked={selected} onChange={e => onChange(e.target.checked)} />
+        {data.title}
+      </label>
     </div>
   )
 }
 
 const FeatureFilter = function({features, selection, onChange}) {
   const setSelection = (feature, value) => onChange({ [feature.id]: value });
+
+  const elements = features.map(f => <Feature key={f.id} data={f} selected={selection[f.id]} onChange={v => setSelection(f, v)} />);
+  const rows = arrayToTable(features, 6);
+  const renderFeature = f => <Feature data={f} selected={selection[f.id]} onChange={v => setSelection(f, v)} />;
+
   return (
-    <div className='feature-filter'>
-      {features.map(f => <Feature key={f.id} data={f} selected={selection[f.id]} onChange={v => setSelection(f, v)} />)}
-    </div>
+    <table className='feature-filter'>
+      <tbody>
+        {rows.map((r, i) => <tr key={i}>{r.map(f => <td key={f.id}>{renderFeature(f)}</td>)}</tr>)}
+      </tbody>
+    </table>
   )
 }
 
