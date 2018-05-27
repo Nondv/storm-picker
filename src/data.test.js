3e3d628e00af7bb42623f4c0bc8449c7b308cc0c
Dmitriy Non <non.dmitriy@gmail.com>
2018-05-27 03:47:52 +0300

    add test for json data

---
+++ src/data.test.js
@@ -0,0 +1,6 @@
+import heroes from '../public/heroes.json';
+import features from '../public/features.json';
+
+it('imports successfuly', () => {
+  // build won't be successful if json cant be parsed
+})
