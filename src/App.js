108631ba9e6818f4519d8fa62b477fb5adf8efd6
PaulSmith220 <kuznetps@gmail.com>
2018-05-31 19:38:16 +0200

    CounterPickFilter integrated to the app, filtering logic updated

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import './App.css';
 import Hero from './ui/Hero'
 import Feature from './entities/Feature'
 import FeatureFilter from './ui/FeatureFilter'
+import CounterPickFilter from './ui/CounterPickFilter';
 import arrayToTable from './util/arrayToTable'
 import sortBy from './util/sortBy'
 import mapToObjects from './util/mapToObjects'
+const mergeObjects = (a, b) => Object.assign({}, a, b)
 
 class App extends Component {
   state = {
-    filterSelection: {}
+    filterSelection: {},
+    filterCounterPickSelection: {},
+    features: [],
+    heroes: [],
+    counterPicks: [],
+    moreThen2FilterMode: false
   }
 
   componentDidMount() {
@@ -18,21 +25,43 @@ class App extends Component {
          .then(response => this.setState({ features: sortBy(mapToObjects(response.data, Feature), 'title') }));
     axios.get('/heroes.json')
          .then(response => this.setState({ heroes: sortBy(response.data, 'name') }));
+    axios.get('/counter-pick.json')
+         .then(response => this.setState({ counterPicks: sortBy(response.data, 'title') }));
+  }
+
+  updateFilters = (c) => {
+    this.setState({ 
+      filterSelection: mergeObjects(this.state.filterSelection, c)
+    })
+  }
+
+  setCounterFilters = (filterCounterPickSelection) => {
+    this.setState({
+      // filterSelection: {},
+      filterCounterPickSelection,
+      moreThen2FilterMode: Object.keys(filterCounterPickSelection).length > 0
+    });
   }
 
   render() {
-    const {filterSelection, features} = this.state;
-    const mergeObjects = (a, b) => Object.assign({}, a, b)
-
+    const {filterSelection, features, counterPicks} = this.state;
+    const rows = this.rowsOfHeroes();
     return (
       <div className="app">
+        <div className="title">
+          What do we need to counter:
+        </div>
+        <CounterPickFilter cases={counterPicks} onChange={this.setCounterFilters} />
+        <div className="title">
+          Additional filters:
+        </div>
         <FeatureFilter features={features || []}
                        selection={{}}
-                       onChange={c => this.setState({ filterSelection: mergeObjects(filterSelection, c) })} />
+                       onChange={this.updateFilters} />
         <table className='app__hero-table'>
           <tbody>
             {
-              this.rowsOfHeroes().map(
+              rows.map(
                 (row, i) => <tr key={i}>
                               {row.map(h => <td key={h.name}><Hero data={h} /></td>)}
                             </tr>
@@ -40,6 +69,7 @@ class App extends Component {
             }
           </tbody>
         </table>
+        {rows.length == 0 && (<div className="empty-message">There is no such hero. Try to make filter strict less.</div>)}
       </div>
     );
   }
@@ -52,22 +82,32 @@ class App extends Component {
   }
 
   filteredHeroes() {
-    const {filterSelection} = this.state;
+    const { filterSelection, filterCounterPickSelection, moreThen2FilterMode } = this.state;
     const heroes = this.state.heroes || [];
     if(!this.isFilterEnabled()) {
       return heroes;
     }
-
+ 
     const requiredFeatures = Object.entries(filterSelection)
                                    .filter(keyvalue => keyvalue[1])
                                    .map(keyvalue => keyvalue[0]);
+    const requiredCounterPickFeatures = Object.entries(filterCounterPickSelection)
+                                   .filter(keyvalue => keyvalue[1])
+                                   .map(keyvalue => keyvalue[0]);                                   
+    const heroFilter = h =>  {
+     
+      if (!moreThen2FilterMode) {
+        return !requiredFeatures.find(f => !h.features[f]);
+      }
 
-    const heroFilter = h => !requiredFeatures.find(f => !h.features[f]);
+      const rfH = requiredCounterPickFeatures.filter(f => h.features[f]);
+      return rfH.length >= 3 && (requiredFeatures.length == 0 || !requiredFeatures.find(f => !h.features[f]));
+    }
     return heroes.filter(heroFilter);
   }
 
   isFilterEnabled() {
-    return -1 !== Object.values(this.state.filterSelection).indexOf(true);
+    return -1 !== Object.values(this.state.filterSelection).indexOf(true) || -1 !== Object.values(this.state.filterCounterPickSelection).indexOf(true);
   }
 }
 
