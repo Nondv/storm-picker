e207d5fac58076e50e58212e63e82a8512ab462a
Dmitriy Non <non.dmitriy@gmail.com>
2018-06-14 04:09:29 +0300

    make hero features list display toggled

--- src/ui/HeroGallery.js
+++ src/ui/HeroGallery.js
@@ -1,32 +1,45 @@
 import './HeroGallery.css'
-import React from 'react';
+import React, { Component } from 'react';
 
-function HeroGallery({heroes}) {
-  return (
-    <div className='hero-gallery grid-x'>
-      {
-        heroes.map(h =>
-          <div className='cell large-4 medium-6 small-12'>
-            <Hero key={h.name} data={h} />
-          </div>
-        )
-      }
-    </div>
-  );
+class HeroGallery extends Component {
+  state = { selected: {} }
+
+  render() {
+    const {heroes} = this.props;
+    const {selected} = this.state;
+
+    const switchSelected = (h) => {
+      selected[h.name] = !selected[h.name];
+      this.forceUpdate();
+    }
+
+    return (
+      <div className='hero-gallery grid-x'>
+        {heroes.map(h => <Hero key={h.name}
+                               data={h}
+                               withFeatures={selected[h.name]}
+                               onClick={() => switchSelected(h)} />)}
+      </div>
+    );
+  }
 }
 
-function Hero({data}) {
+function Hero({data, withFeatures, onClick}) {
   const images = data.images || {};
   const features = data.features || {};
+  const sizeClasses = withFeatures ? 'large-4 medium-6 small-12' : 'large-2 medium-3 small-6';
 
   return (
-    <div className='hero-gallery__hero'>
+    <div onClick={onClick} className={`hero-gallery__hero cell ${sizeClasses}`}>
       <img className='hero-gallery__bust' alt={data.name} src={images.bust} />
-      <div className='hero-gallery__features'>
-        <ul>
-          {Object.keys(features).map(f => <li key={f}>{f}</li>)}
-        </ul>
-      </div>
+      {
+        withFeatures &&
+        <div className='hero-gallery__features'>
+          <ul>
+            {Object.keys(features).map(f => <li key={f}>{f}</li>)}
+          </ul>
+        </div>
+      }
     </div>
   )
 }
