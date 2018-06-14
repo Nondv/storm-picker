c200262429ec719162781898205868e71dd0fea8
Dmitriy Non <non.dmitriy@gmail.com>
2018-06-14 03:01:21 +0300

    [refactoring] move Hero to HeroGallery

--- src/ui/HeroGallery.js
+++ src/ui/HeroGallery.js
@@ -1,19 +1,26 @@
 import './HeroGallery.css'
-import Hero from './Hero'
 import React from 'react';
 
 function HeroGallery({heroes}) {
   return (
     <div className='hero-gallery grid-x'>
-      {
-        heroes.map(h =>
-          <div key={h.name} className='hero-gallery__hero cell large-2 medium-3 small-6'>
-            <Hero data={h} />
-          </div>
-        )
-      }
+      {heroes.map(h => <Hero key={h.name} data={h} />)}
     </div>
   );
+}
+
+function Hero({data}) {
+  const images = data.images || {};
+  const features = data.features || {};
+
+  return (
+    <div className='hero-gallery__hero cell large-2 medium-3 small-6'>
+      <img className='hero-gallery__bust' alt={data.name} src={images.bust} />
+      <ul>
+        {Object.keys(features).map(f => <li key={f}>{f}</li>)}
+      </ul>
+    </div>
+  )
 }
 
 
